f626625b1a62bfade14c4c6c7a90a12daef5c055
Po-Yuan Wang <andrew.wang@onedegree.hk>
2022-09-10 21:58:49 +0800

    lab3 done

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,63 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+
+  @Test
+  public void testThreeAddThreeRemove(){
+    AListNoResizing<Integer> a = new AListNoResizing<Integer>();
+    BuggyAList<Integer> b = new BuggyAList<Integer>();
+  
+    for (int i = 0; i <= 2; i++ )
+    {
+      a.addLast(i);
+      b.addLast(i);
+    }
+
+    for (int i = 0; i<= 2; i++)
+    {
+      assertEquals(a.removeLast(), b.removeLast());
+    }
+
+  }
+
+  @Test
+  public void randomizedTest()
+  {
+    AListNoResizing<Integer> a = new AListNoResizing<Integer>();
+    BuggyAList<Integer> b = new BuggyAList<Integer>();
+  
+    int N = 5000;
+    for (int i = 0; i<= N; i++)
+    {
+      int operationNumber = StdRandom.union(0,4);
+      if (operationNumber == 0)
+      {
+          int randVal = StdRandom.uniform(0, 100);
+          a.addLast(randVal);
+          b.addLast(randVal);
+      }
+      else if (operationNumber == 1)
+      {
+        int size_a = a.size();
+        int size_b = b.size();
+        assertEquals(size_a, size_b);
+      }
+      else if (a.size() > 0)
+      {
+        if(operationNumber == 2)
+        {
+          int a_last = a.getLast();
+          int b_last = b.getLast();
+          assertEquals(size_last, size_last);    
+        } 
+        else if (operationNumber == 3)
+        {
+          int a_removelast = a.removeLast();
+          int b_removelast = b.removeLast();
+          assertEquals(a_removelast, b_removelast);      
+        }
+      }
+    
+    }
+  }
 }
